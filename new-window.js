017199cc1f74d6db82c6a4a909374213156258f3
monsteronfire <monsteronfire@gmail.com>
2016-07-26 08:56:36 +0800

    Add examples for window binding

--- new-window.js
+++ new-window.js
@@ -13,4 +13,17 @@ var zebra = new Animal('black and white', 'Zorro', 'Zebra');
 
 
 
+//Window Binding Examples
+var sayAge =  function() {
+  console.log(this.age);
+};
 
+//Invoking the sayAge() function like below will return 'undefined'
+//because if you invoke a function that uses the 'this' keyword but doesn't have anything left of the dot
+//the 'this' keyword is going to default to the window object
+sayAge();
+
+//So if we add a property of 'age' to the window object
+//and invoke sayAge() again, it should log the window.age you set on window
+window.age = 35;
+sayAge();
